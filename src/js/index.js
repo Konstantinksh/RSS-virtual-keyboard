0e3fe84b4bfb6c4606c418ce6488bf43eccfbf3b
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-06 12:02:04 +0300

    feat: start developing

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,58 @@
-import '../scss/style.scss'
+console.log('start')
 
-function foo(e) {
-  console.log(e);
+
+const Keyboard = {
+  elements: {
+    main: null,
+    keysContainer: null,
+    keys: []
+  },
+
+  eventHandlers: {
+    oninput: null,
+    onclose: null
+  },
+
+  properties: {
+    value: "",
+    capsLock: false
+  },
+
+  init() {
+    console.log('init')
+    this.elements.main = document.createElement('div');
+    this.elements.keysContainer = document.createElement('div');
+
+    this.elements.main.classList.add('keyboard');
+    this.elements.keysContainer.classList.add('keyboard__conteiner');
+
+    this.elements.main.append(this.elements.keysContainer);
+    document.body.append(this.elements.main)
+  },
+
+  _createKeys() {
+
+  },
+
+  _trigerEvent(handlerName) {
+    console.log("Event Triggered! Name:" + handlerName)
+  },
+
+  _toggleCapsLock() {
+    console.log("Caps Lock Toggled!")
+  },
+
+  open(initialValue, oninput, onclose) {
+
+  },
+
+  close() {
+
+  }
 }
 
-foo(4);
+window.addEventListener("DOMContentLoaded", function() {
+  Keyboard.init();
+})
+
+console.log('end')
