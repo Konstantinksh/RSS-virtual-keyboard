6510510e000abd0480faa044d9c97b67b96636c5
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-08 23:14:10 +0300

    feat: add functionality for mouse events and keyboardevents

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,33 @@
 console.log('start')
 
-import '../scss/style.scss'
+import '../scss/style.scss';
+import { keysData } from './KeysData';
+import { KeyButton } from './KeyButton';
+import { CustomMouseEvent } from './CustomMouseEvent';
 
+
+window.addEventListener("DOMContentLoaded", function() {
+  Keyboard.init();
+  Keyboard._listen();
+})
 
 const Keyboard = {
   elements: {
     main: null,
     mainContainer: null,
     keysContainer: null,
-    keys: []
-  },
-
-  eventHandlers: {
-    oninput: null,
-    onclose: null
+    keys: [],
+    activeButtons: new Set(),
+    inputArea: document.querySelector('textarea')
   },
 
   properties: {
     value: "",
-    capsLock: false
+    capsState: false,
+    shiftState: false,
+    ctrlState: false,
+    altState: false,
+    langState: 'Eng'
   },
 
   init() {
@@ -31,65 +40,189 @@ const Keyboard = {
     this.elements.mainContainer.classList.add('keyboard__conteiner');
     this.elements.keysContainer.classList.add('keyboard__keys', 'key', '_wrapper');
 
-    this.elements.keysContainer.append(this._createKeys())
+    this.elements.keysContainer.append(this._createKeys());
     this.elements.mainContainer.append(this.elements.keysContainer);
     this.elements.main.append(this.elements.mainContainer);
-    document.body.append(this.elements.main)
+    document.body.append(this.elements.main);
+  },
+
+  refresh() {
+    console.log('referesh')
+    this.elements.keysContainer = document.querySelector('.keyboard__keys');
+    this.elements.keysContainer.innerHTML = '';
+    this.elements.keysContainer.append(this._createKeys())
+  },
+
+  input() {
+
   },
 
   _createKeys() {    
     const fragment = document.createDocumentFragment();
-    const keyLayout = [
-        "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
-        "Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "DEL",
-        "Caps Lock", "A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER",
-        "Shift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "&#9650", "Shift",
-        "Ctrl", "Win", "Alt", "space", "Alt", "Ctrl", "&#9668", "&#9660", "&#9658"
-    ];
 
-    function createButtonHTML(key) {
-      let keyButton = document.createElement('button');
-
-      keyButton.setAttribute('type', 'button');
-      keyButton.classList.add('key__container');
-
-      keyButton.innerHTML = `${key}`;
-
-      return keyButton
-    }
-
-    keyLayout.forEach(key => {
-      const keyElement = createButtonHTML(key);
-
-      fragment.append(keyElement)
+    keysData.forEach(elem => {
+      const keyElement = new KeyButton(elem, this.properties);
+      const keyElementHTML = keyElement.createButton();
+      setKeyClasses(keyElement.code, keyElementHTML);
+      fragment.append(keyElementHTML);
     })
 
-    function setKeyClasses(key) {
-      
+    function setKeyClasses(key, keyElement) {
+      // console.log(key, keyElement)
+      if (Keyboard.elements.activeButtons.has(key)) {
+        keyElement.classList.add('key_active');
+      } else if (Keyboard.elements.activeButtons.has(key)) {
+        keyElement.classList.add('key_active');
+      }
+      switch (key) {
+        case "Backspace":
+        case "Enter":
+          keyElement.classList.add('key_blacked', 'key_size-l')
+          break;
+        case "ShiftLeft":
+        case "ShiftRight":
+          keyElement.classList.add('key_blacked', 'key_size-xl')
+          break;
+        case "CapsLock":
+          keyElement.classList.add('key_blacked', 'key_size-xxm')
+          break;
+        case "Tab":
+        case "Delete":
+        case "Fn":
+          keyElement.classList.add('key_blacked', 'key_size-m')
+          break;
+        case "ControlLeft":
+        case "ControlRight":
+          keyElement.classList.add('key_blacked', 'key_size-xm')
+          break;
+        case "ArrowUp":
+        case "ArrowRight":
+        case "ArrowLeft":
+        case "ArrowDown":
+        case "AltLeft":
+        case "AltRight":
+        case "Backquote":
+          keyElement.classList.add('key_blacked')
+          break;
+        case "Space":
+          keyElement.classList.add('key_size-xxl')
+          break;
+      }
     }
-
     return fragment
   },
 
-  _trigerEvent(handlerName) {
-    console.log("Event Triggered! Name:" + handlerName)
+  _listen(){
+    document.body.addEventListener('keydown', this._trigerKeyboardEvent);
+    document.body.addEventListener('keyup', this._trigerKeyboardEvent);
+    // console.log(this.elements.inputArea)
+    this.elements.inputArea.addEventListener("focusout", () => this.elements.inputArea.focus());
+    this.elements.keysContainer.addEventListener('mousedown', this._trigerMouseEvent)
+    this.elements.keysContainer.addEventListener('mouseup', this._trigerMouseEvent)
   },
 
-  _toggleCapsLock() {
-    console.log("Caps Lock Toggled!")
+  _trigerMouseEvent(event){
+    let newMouseEvent = new CustomMouseEvent(event);
+
+    if (newMouseEvent.isTargetButton) {
+      newMouseEvent.getInnerType(keysData);
+      newMouseEvent.getNewEventValue();
+      newMouseEvent.getNewEventType();
+
+      if (event.type === 'mousedown') {
+        Keyboard.elements.inputArea.value += newMouseEvent.newEventValue;
+        Keyboard.elements.keysContainer.addEventListener('mouseout', Keyboard._trigerMouseEvent);
+      } else if (event.type === 'mouseup' || event.type === 'mouseout') {
+        Keyboard.elements.keysContainer.removeEventListener('mouseout', Keyboard._trigerMouseEvent);
+      }
+      // console.log(newMouseEvent)
+      let newButtonEvent = new KeyboardEvent(newMouseEvent.newEventType, {code: newMouseEvent.newEventCode})
+      document.body.dispatchEvent(newButtonEvent)
+    }
   },
 
-  open(initialValue, oninput, onclose) {
-
+  _trigerKeyboardEvent(event) {
+    // console.log("Event Triggered! Name:" + event.code)
+    Keyboard._makeButtonActive(event)
+    switch (event.code) {
+      case "ShiftLeft":
+      case "ShiftRight":
+        Keyboard._toggleShiftState(event);
+        break;
+      case "CapsLock":
+        Keyboard._toggleCapsState(event);
+        break;
+      case "AltLeft":
+      case "AltRight":
+        Keyboard._toggleAltState(event);
+        Keyboard._toggleLangState();
+        break;
+      case "ControlLeft":
+      case "ControlRight":
+        Keyboard._toggleControlState(event);
+        Keyboard._toggleLangState();
+        break;
+    }
   },
 
-  close() {
+  _toggleCapsState(event) {
+    if (event.type === 'keydown') {
+      this.properties.capsState === true ? this.properties.capsState = false : this.properties.capsState = true;
+    } else {return}
+    Keyboard.refresh();
+    console.log(this.properties.capsState, event.code);
+  },
 
+  _toggleShiftState(event) {
+    if (event.type === 'keydown') {
+      this.properties.shiftState === true ? this.properties.shiftState = false : this.properties.shiftState = true;
+    } else if (event.type === 'keyup') {
+      this.properties.shiftState === true ? this.properties.shiftState = false : this.properties.shiftState = true;
+    }
+    Keyboard.refresh();
+    // console.log(this.properties.shiftState);
+  },
+
+  _toggleControlState(event) {
+    // console.log("Ctrl Toggled!")
+    if (event.type === 'keydown') {
+      this.properties.ctrlState = true
+    } else if (event.type === 'keyup') {
+      this.properties.ctrlState = false;
+    }
+    // console.log(this.properties.ctrlState);
+  },
+
+  _toggleAltState(event) {
+    // console.log("Alt Toggled!")
+    if (event.type === 'keydown') {
+      this.properties.altState = true
+    } else if (event.type === 'keyup') {
+      this.properties.altState = false;
+    }
+    // console.log(this.properties.altState);
+  },
+
+  _toggleLangState() {
+    console.log(this.properties.altState, this.properties.ctrlState)
+    if (this.properties.altState && this.properties.ctrlState) {
+      this.properties.langState === 'Eng' ? this.properties.langState = 'Ru' : this.properties.langState = 'Eng';
+      // console.log("Lang Toggled!");
+      Keyboard.refresh();
+    }
+  },
+
+  _makeButtonActive(event) {
+    if (event.type === 'keydown') {
+      Keyboard.elements.activeButtons.add(event.code);
+      Keyboard.refresh();
+      console.log(Keyboard.elements.activeButtons);
+    } else if (event.type === 'keyup') {
+      Keyboard.elements.activeButtons.delete(event.code);
+      Keyboard.refresh();
+      console.log(Keyboard.elements.activeButtons)
+    }
   }
 }
-
-window.addEventListener("DOMContentLoaded", function() {
-  Keyboard.init();
-})
 
 console.log('end')
