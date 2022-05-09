6293a21fadaaa076e99ca3def4bd2e36dc042eb4
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-09 20:29:18 +0300

    docs: update gitignore and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  output: {
-    filename: '[name].[hash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    port: 4200,
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: './src/index.html'
-    }),
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: path.resolve(__dirname, 'src/assets/icons/favicon.png'),
-          to: path.resolve(__dirname, 'dist/assets/icons')
-        },
-      ],
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[hash].css'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  }
-}
+    mode: 'development',
+    entry: './src/js/index.js',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        port: 4200,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/assets/icons/favicon.png'),
+                    to: path.resolve(__dirname, 'dist/assets/icons'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+        ],
+    },
+    clean: {
+        keep: /\.git/,
+    },
+};
