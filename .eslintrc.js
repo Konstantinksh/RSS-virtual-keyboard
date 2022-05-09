194b2712197ea8c6f13bdf975c786ea425ad9fb5
Kanstantsin Krauchuk <konstantinksh@gmail.com>
2022-05-09 19:51:45 +0300

    fix: eslint errors fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
         'browser': true,
         'es2021': true
     },
-    'extends': 'eslint:recommended',
+    'extends': ["airbnb-base"],
     'parserOptions': {
         'ecmaVersion': 'latest',
         'sourceType': 'module'
